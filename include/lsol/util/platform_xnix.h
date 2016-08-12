9b03f90cec18742c26c6f4006584bddf6226972c
Yue Wu <yuewu@outlook.com>
2016-08-12 22:52:31 +0800

    use c++11 time

--- include/lsol/util/platform_xnix.h
+++ include/lsol/util/platform_xnix.h
@@ -9,19 +9,10 @@
 #ifndef LSOL_UTIL_PLATFORM_XNIX_H__
 #define LSOL_UTIL_PLATFORM_XNIX_H__
 
-#include <sys/time.h>
-#include <sys/types.h>
-
 namespace lsol {
 
 inline FILE* open_file(const char* path, const char* mode) {
   return fopen(path, mode);
-}
-
-inline double get_current_time() {
-  struct timeval tim;
-  gettimeofday(&tim, NULL);
-  return tim.tv_sec + tim.tv_usec / 1000000.0;
 }
 
 }  // namespace lsol
